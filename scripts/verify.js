ec0ee40c9d0dfde63785677dcd7d6161931144b5
kusergiy <kustovskuysergiy@gmail.com>
2022-09-03 18:42:06 +0300

    add new contract v2

--- scripts/verify.js
+++ scripts/verify.js
@@ -4,7 +4,7 @@ require('@nomiclabs/hardhat-etherscan');
 
 async function main() {
     await hre.run('verify:verify', {
-        address: '0xE019591Ca581826d44681502051b7b8Eb042e3e0',
+        address: '0x591F3D8C4d1B810e5fA7d088aC03C884A5bF44A4',
         constructorArguments: [],
     });
 }
